677c03cc5211409491b61cc198dbfb277ee53181
Jeralin <jeralinflores4@gmail.com>
2024-02-26 19:32:17 -0600

    Jeralin Flores

---
+++ main.py
@@ -0,0 +1,9 @@
+
+def Saludar_Usuario():
+    Nombre_Usuario = input("Escribe tu nombre: ")
+
+    Mensaje = f" ¡Hello {Nombre_Usuario}! welcome to our personalized program, ¡enjoy the process! "
+    print(Mensaje)
+
+
+Saludar_Usuario()
