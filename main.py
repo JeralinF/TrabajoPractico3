36ca4c3ec81cfbafafdf0785502475e6c62e1a33
rebeca <rebecamedinadiaz@gmail.com>
2024-02-28 22:46:17 -0600

    funcion-numeros-agregados

--- main.py
+++ main.py
@@ -7,3 +7,23 @@ def Saludar_Usuario():
 
 
 Saludar_Usuario()
+
+def contador_de_numeros():
+    try:
+
+        numero_ingresado = int(input("Ingrese un número entero: "))
+
+
+        if numero_ingresado <= 0:
+            print("Por favor, ingrese un número entero positivo.")
+            return
+
+
+        for i in range(1, numero_ingresado + 1):
+            print(i)
+
+    except ValueError:
+        print("Por favor, ingrese un número entero válido.")
+
+
+contador_de_numeros()
